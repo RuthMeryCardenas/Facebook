ad33301c907dbf80b6c541b72e8054d0df44149c
RuthMery <ruth.cardenas.perez@gmail.com>
2017-10-01 13:24:49 -0500

    login - cargando usuarios

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-$().ready( () => {
-  // my code will go here
+$().ready(() => {
+  const users = [{id: 1, email: "ruth.cardenas.perez@gmail.com", password: "mery"},
+                 {id: 2, email: "vania@laboratoria.la", password: "vania"}];
+  insertItem('users', users);
 });
+
+const insertItem = (key, value) => {
+  if (selectItem(key) === null) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+const selectItem = (key) => JSON.parse(localStorage.getItem(key));
+
+const deleteItem = (key) => {
+  localStorage.removeItem(key);
+  return  true;
+}
--- js/data.js
+++
@@ -1,4 +0,0 @@
-export default [
-  {id: 1, email: "ruth.cardenas.perez@gmail.com", password: "mery"},
-  {id: 2, email: "vania@laboratoria.la", password: "vania"}
-];
